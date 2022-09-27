a96edf7eb2816223e20bc5a9e3ec68a2aad53562
saurabh <saurabhkatwal@gmail.com>
2022-09-27 13:18:20 +0530

    added on focus event to input to restore the data

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,24 @@ import data from "../data/data";
 import Toggler from "./Toggler";
 import Cards from "./Cards";
 import { useState } from "react";
+//list of house data
 let houseData=data.houses;
+//object of different houses
 let housePeopleObj=houseData.reduce((acc,curr)=>{
   acc[curr.name]={};
   acc[curr.name]["people"]=curr.people;
   return acc;
 },{})
+//list of housenames
 let houseNames=houseData.map(houseObj=>{
   return houseObj.name;
 })
 // console.log(houseData);
+//array containing array of people in each house
 let peopleArrays = houseData.map(house => {
   return house["people"];
 })
+//list of people with their information 
 let peopleList=[];
 peopleArrays.forEach(peopleArray=>{
 peopleArray.forEach(people=>{
@@ -30,8 +35,11 @@ function clickHandler(e){
   let text=e.target.innerText;
   setList(housePeopleObj[text].people);
 }
+function inputFocusHandler(){
+setList(peopleList);
+}
   return <div className="root">
-    <NavBar/>
+    <NavBar inputFocusHandler={inputFocusHandler}/>
     <Toggler clickHandler={clickHandler} houses={houseNames}/>
     <Cards people={list}/>
   </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import "./css/NavBar.css"
-export default function NavBar() {
+export default function NavBar(props) {
     
   return (
     <div className='nav'>
       <h1>People of GOT 👑</h1>
-      <input type="text"/>
+      <input type="text" onFocus={props.inputFocusHandler}/>
     </div>
   )
 }
